dc0ab1692bca79ea949efd59906057c22b379852
Lars Johan Folde <ljfolde@gmail.com>
2025-02-12 00:10:41 +0100

    Tweaked dates
    The notification now includes the start date and end date of the competition. It will also correctly set the timezone. For now it only uses Europe/Oslo, but this will be changed to be dynamic if it is ever relevant

--- mailer.py
+++ mailer.py
@@ -10,8 +10,8 @@ def notify(subscriber: EmailSubscriber, competition: Competition) -> None:
     body = f"""<html>
     <body>
         <p>Dear {subscriber.name},</p>
-        <p>{competition.name} has just been announced on the WCA Website, and will take place in {competition.city} on {competition.start_date.strftime('%A, %B %d')}.</p>
-        <p>Registration for this competition is open from {competition.registration_open.strftime('%H:%M %A, %B %d')} to {competition.registration_close.strftime('%H:%M %A, %B %d')}.</p>
+        <p>{competition.name} has just been announced on the WCA Website, and will take place in {competition.city} on {competition.format_dates()}.</p>
+        <p>Registration for this competition is open from {competition.registration_open_with_timezone('Europe/Oslo').strftime('%H:%M %A, %B %d')} to {competition.registration_close_with_timezone('Europe/Oslo').strftime('%H:%M %A, %B %d')}.</p>
         <p>You can register for this competition <a href="https://worldcubeassociation.org/competitions/{competition.id}">here</a>.</p>
         <p>Best regards,<br>{competition.delegates}</p>
     </body>
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from datetime import datetime
 import pandas as pd
+import pytz
 
 @dataclass
 class Competition:
@@ -8,6 +9,7 @@ class Competition:
     name: str
     city: str
     start_date: datetime
+    end_date: datetime
     registration_open: datetime
     registration_close: datetime
     delegates: str
@@ -19,25 +21,44 @@ class Competition:
             name = api_response['name'],
             city = api_response['city'],
             start_date = datetime.fromisoformat(api_response['start_date'].replace('Z', '+00:00')).replace(tzinfo=None),
+            end_date = datetime.fromisoformat(api_response['end_date'].replace('Z', '+00:00')).replace(tzinfo=None),
             registration_open = datetime.fromisoformat(api_response['registration_open'].replace('Z', '+00:00')).replace(tzinfo=None),
             registration_close = datetime.fromisoformat(api_response['registration_close'].replace('Z', '+00:00')).replace(tzinfo=None),
-            delegates = ", ".join([delegate['name'] for delegate in api_response['delegates']])
+            delegates = ', '.join([delegate['name'] for delegate in api_response['delegates']])
         )
 
     @classmethod
     def from_series(cls, row: pd.Series):
         return cls(
-            id = str(row["id"]),
-            name = str(row["name"]),
-            city = str(row["city"]),
-            start_date = row["start_date"],
-            registration_open = row["registration_open"],
-            registration_close = row["registration_close"],
-            delegates = str(row["delegates"])
+            id = str(row['id']),
+            name = str(row['name']),
+            city = str(row['city']),
+            start_date = row['start_date'],
+            end_date = row['end_date'],
+            registration_open = row['registration_open'],
+            registration_close = row['registration_close'],
+            delegates = str(row['delegates'])
         )
 
     def __eq__(self, other):
         return self.id == other.id
+
+    def format_dates(self) -> str:
+        if self.start_date == self.end_date:
+            return self.start_date.strftime("%b %d, %Y")
+
+        if self.start_date.year == self.end_date.year and self.start_date.month == self.end_date.month:
+            return f"{self.start_date.strftime('%b %d')} - {self.end_date.strftime('%d, %Y')}"
+        else:
+            return f"{self.start_date.strftime('%b %d, %Y')} - {self.end_date.strftime('%b %d, %Y')}"
+
+    def registration_open_with_timezone(self, timezone: str) -> datetime:
+        tz = pytz.timezone(timezone)
+        return self.registration_open.replace(tzinfo=pytz.utc).astimezone(tz)
+
+    def registration_close_with_timezone(self, timezone: str) -> datetime:
+        tz = pytz.timezone(timezone)
+        return self.registration_close.replace(tzinfo=pytz.utc).astimezone(tz)
 
 @dataclass
 class EmailSubscriber:
